12c19ef1c6f1dbf3b65fddb4ee43a1ac5282e226
Dave <Dave1227>
2016-01-04 23:00:47 -0500

    solution

--- k_palindrome.py
+++ k_palindrome.py
@@ -1,1 +1,12 @@
 # http://www.careercup.com/question?id=6287528252407808
+# difficulty: easy
+# category: recursion
+
+
+def solution(word, k):
+    if len(word) <= 1:
+        return True
+    elif word[0] != word[-1]:
+        return solution(word[1:], k - 1) or solution(word[:-1], k - 1)
+    else:
+        return solution(word[1:-1], k)
