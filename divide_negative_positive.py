7cdab8fc1f578ec9abcf7e5edcfa8b8b477ed735
dave322942780 <weihang.li@mail.utoronto.ca>
2016-02-24 16:25:59 -0500

    Update divide_negative_positive.py

--- divide_negative_positive.py
+++ divide_negative_positive.py
@@ -1,5 +1,9 @@
 # http://www.careercup.com/question?id=19286747
-
+# Given an array of integers. Find two disjoint contiguous sub-arrays such
+# that the absolute difference between the sum of two sub-array is maximum.
+# * The sub-arrays should not overlap.
+# eg- [2 -1 -2 1 -4 2 8] ans - (-1 -2 1 -4) (2 8), diff = 16
+# I gave him o(n^2) algorithm but he was not satisfied.
 
 def solution(lst):
     i = 0
