a89edff5681347a966cd9492e14fce6b0dba8f92
Dave <Dave1227>
2016-10-26 21:52:11 -0400

    solution

---
+++ tests/test_disjoint_lst_max_abs_diff.py
@@ -0,0 +1,4 @@
+# https://careercup.com/question?id=19286747
+
+def disjoint_lst_max_abs_diff(lst):
+    pass
---
+++ tests/test_get_diagonal.py
@@ -0,0 +1,22 @@
+# https://careercup.com/question?id=19286747
+import unittest
+
+from disjoint_lst_max_abs_diff import disjoint_lst_max_abs_diff
+
+
+class DisjointLstMaxAbsDiffTestCase(unittest.TestCase):
+    def test_disjoint_lst_max_abs_diff(self):
+        lst = [2, -1, -2, 1, -4, 2, 8]
+        self.assertEqual(disjoint_lst_max_abs_diff(lst), [[-1, -2, 1, -4], [2, 8], 16])
+
+    def test_disjoint_lst_max_abs_diff_2(self):
+        lst = [2, -1, -2, 1, -4, 2, 8, -1]
+        self.assertEqual(disjoint_lst_max_abs_diff(lst), [[-1, -2, 1, -4], [2, 8], 16])
+
+    def test_disjoint_lst_max_abs_diff_3(self):
+        lst = [-1, -2, 1, -4, 2, 8, -1]
+        self.assertEqual(disjoint_lst_max_abs_diff(lst), [[-1, -2, 1, -4], [2, 8], 16])
+
+    def test_disjoint_lst_max_abs_diff_3(self):
+        lst = [-1, -2, 1, -4, 2, 8]
+        self.assertEqual(disjoint_lst_max_abs_diff(lst), [[-1, -2, 1, -4], [2, 8], 16])
