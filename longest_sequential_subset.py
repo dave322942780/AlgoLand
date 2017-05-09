01c22671645238dbb9b4c4872cdd55dac8e19946
davelidev <weihang.li@mail.utoronto.ca>
2017-05-08 22:34:32 -0400

    Update longest_sequential_subset.py

--- longest_sequential_subset.py
+++ longest_sequential_subset.py
@@ -1,21 +1,22 @@
 # https://www.careercup.com/question?id=11070934
 
 def solution(lst):
-    max_item = max(lst) + 1
-    visited = [False] * max_item
-    for item in lst:
-        visited[item] = True
-    max_seq = 0
-    cur_max_seq = 0
-    max_rang = None
-    for i in range(len(visited)):
-        item = visited[i]
-        if item:
-            cur_max_seq += 1
-            if cur_max_seq > max_seq:
-                max_seq = cur_max_seq
-                max_rang = (i - cur_max_seq + 1, i + 1)
+    boundary_record = {}
+    max_range = None
+    max_length = 0
+    
+    for elem in input_set:
+        if elem in boundary_record:
+            pass
         else:
-            cur_max_seq = 0
-
-    return range(max_rang[0], max_rang[1])
+            left_most = right_most = elem
+            if elem - 1 in boundary_record:
+                left_most = boundary_record[elem - 1]
+            if elem + 1 in boundary_record:
+                right_most = boundary_record[elem + 1]
+            if right_most - left_most > max_length:
+                max_length = right_most - left_most
+                max_range = [left_most, right_most + 1]
+            boundary_record[left_most] = right_most
+            boundary_record[right_most] = left_most
+    return None if not max_range else range(max_range[0], max_range[1])
